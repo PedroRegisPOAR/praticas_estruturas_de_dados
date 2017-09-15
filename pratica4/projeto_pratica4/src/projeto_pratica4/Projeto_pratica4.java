2ede4d78053f16fc6b53d2ee5845453887bf0998
Pedro Regis <pedroalencarregis@hotmail.com>
2017-09-15 02:25:24 -0300

    Inicio da pratica4.

---
+++ pratica4/projeto_pratica4/src/projeto_pratica4/Projeto_pratica4.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projeto_pratica4;
+
+import java.util.LinkedList;
+
+
+public class Pilha
+{
+    String name = "Pedro";
+    
+    //conteudo = 
+    
+    private static boolean estaVazia(LinkedList<X> p)
+    {
+        return p.size() == 0;
+    }
+
+    private static void empilha(LinkedList<X> x)
+    {
+        // 
+    }
+
+    private static X desempilha(LinkedList<X> p)
+    {
+        // return 
+    }    
+
+    private static X topo(LinkedList<X> p)
+    {
+        //return 
+    }    
+    
+}
+
+/**
+ *
+ * @author PEDRO REGIS
+ */
+public class Projeto_pratica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("Hello Wolrd!!!");
+    }
+}
